9cc34752236b46cc1fe0f407176da51aa20f2750
nawt2001 <75483969+nawt2001@users.noreply.github.com>
2023-01-19 10:13:40 +0100

    Doxygen Main.py

--- MicroPythoncode/main.py
+++ MicroPythoncode/main.py
@@ -1,3 +1,4 @@
+///\author Twan Wieggers
 import time
 import pycom
 import machine
@@ -20,62 +21,84 @@ app_eui = ubinascii.unhexlify('0000000000000000')
 app_key = ubinascii.unhexlify('D85E148856D86DB45DB888CAB77B96FC')
 dev_eui = ubinascii.unhexlify('70B3D57ED00581D2')
 max_humdity = 255
-send_delay = 60
+send_delay = 300
 temp_offset = -7
-humd_offset = 0.4
+humd_offset = 3.2
 
 dht = SI7006A20()
 li = LTR329ALS01()
 alt = MPL3115A2(mode=PRESSURE)
 pycom.heartbeat(False)
 
-#this is the function for making the connection to the things network application.
-#it uses the dev_eui, app_eui and the app_key from the application on the things network
-#it keeps trying to connect to the network
+## this is the function for making the connection to the things network application.
+#
+# @brief it uses the dev_eui, app_eui and the app_key from the application on the things network
+# it keeps trying to connect to the network
 def lora_connection():
     lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
     while not lora.has_joined():
-        set_led_red()
+        # set_led_red()
         time.sleep(2.5)
         print('Not yet joined...')
 
-    set_led_green()
+    # set_led_green()
     print('Joined')
     s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
     s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
     s.setblocking(True)
     return(s)
-#this function uses the already made objects from the library and call there functions to receive the right data
+
+##
+# @brief This function uses the already made objects from the library and calls their functions to receive the right data
+#
+# @return The function returns a tuple of (pressure, temperature, light, humidity)
 def receive_sensor_value():
+    """
+    Retrieve sensor values for pressure, temperature, light, and humidity
+
+    Returns:
+    Tuple of floats: (pressure, temperature, light, humidity)
+    """
     pressure = alt.pressure()
     temp = dht.temperature()
     light = li.lux()
     humdity = dht.humidity()
     return (pressure,temp,light,humdity)
-#the function recieves the raw pressure and do it times 0.01 to get the value in mBar.
-#for sending the data over lora we can use max of 255 to fit it in 1 bytes sow we do -900
-#in the decoder we add the 900 back to it.
+
+## the function recieves the raw pressure and do it times 0.01 to get the value in mBar.
+# @param pressure the raw pressure value
+# for sending the data over lora we can use max of 255 to fit it in 1 bytes sow we do -900
+# in the decoder we add the 900 back to it.
 def calculate_pressure(pressure):
     pres = pressure*0.01 -900
     pres = int(pres)
     return(pres)
-#scales the temperature value and offsets it by a specified amount. It also handles negative temperatures.
-#it handles these negative temperature by setting a bit to 1 if it is negative
+
 def calculate_temp(temp):
+    """
+    Scales the temperature value and offsets it by a specified amount. It also handles negative temperatures.
+    Handles negative temperature by setting a bit to 1 if it is negative.
+    :param temp: The raw temperature value
+    :return: (fulltemp, decimaltemp, minus)
+    """
     if(temp < 0):
         minus = 1
         temp = temp * (-1)
         fulltemp = int(temp)
         decimaltemp = int((temp - fulltemp)*10)
     else:
-        fulltemp = int(temp)
+        fulltemp = int(temp) + temp_offset
         decimaltemp = int((temp - fulltemp)*10)
         minus = 0
     fulltemp = fulltemp + temp_offset
     return(fulltemp,decimaltemp,minus)
-#scales the light value to fit in 1 byte
-#it use the logarithmic scale because lux is a logarithmic value
+
 def calculate_light(light):
+    """
+    Scales the light value to fit in 1 byte. It uses a logarithmic scale because lux is a logarithmic value
+    @param light: the raw light value
+    @return: the scaled light value
+    """
     lightvalue = int(light)
     if(lightvalue < 123):
         lightvalue = lightvalue
@@ -84,17 +107,40 @@ def calculate_light(light):
         if(lightvalue > 255):
             lightvalue = 255
     return(lightvalue)
-#scales the humidity value to fit in 1 byte and offsets it by a specified amount.
+
 def calculate_humd(humdity):
+    """
+    Scales the humidity value to fit in 1 byte and offsets it by a specified amount.
+
+    @param humdity: The raw humidity value from the sensor
+    @type humdity: float
+
+    @return: The scaled and offset humidity value
+    @rtype: int
+    """
     humdity = humdity * humd_offset
     humdity = int(humdity)
-    # if(humdity > 255):
-    #     humdity = 100
-    # else:
-    #     humdity = int(humdity/(max_humdity/100))
     return(humdity)
-#creates a payload of sensor data to be sent over the LoRa network.
+
 def construct_payload(fulltemp,decimaltemp,pressure,humdity,light,minus):
+    """
+    Creates a payload of sensor data to be sent over the LoRa network.
+
+    @param fulltemp: the full temperature value, scaled and offset
+    @type fulltemp: int
+    @param decimaltemp: the decimal part of the temperature value
+    @type decimaltemp: int
+    @param pressure: the pressure value, scaled and offset
+    @type pressure: int
+    @param humdity: the humidity value, scaled and offset
+    @type humdity: int
+    @param light: the light value, scaled and offset
+    @type light: int
+    @param minus: a flag indicating if the temperature is negative
+    @type minus: int
+    @return: the payload to be sent over the LoRa network
+    @rtype: bytes
+    """
     fulltempstruct = ustruct.pack('b',fulltemp)
     decitempstruct = ustruct.pack('b',decimaltemp)
     presstruct = ustruct.pack('b',pressure)
@@ -104,12 +150,22 @@ def construct_payload(fulltemp,decimaltemp,pressure,humdity,light,minus):
     payload = presstruct + fulltempstruct + decitempstruct + lightstruct + humditystruct + minusstruct
     return(payload)
 
-#set the led to red
-def set_led_red():
-    pycom.rgbled(0x7f0000)
-#set the led to green
-def set_led_green():
-    pycom.rgbled(0x007f00)
+
+    ##
+    # set the led to red
+    def set_led_red():
+        """
+        This function is used to set the LED to red.
+        """
+        pycom.rgbled(0x7f0000)
+    ##
+    # set the led to green
+    def set_led_green():
+        """
+        This function is used to set the LED to green.
+        """
+        pycom.rgbled(0x007f00)
+
 
 
 while 1:
